c35b05bcaf9a4f56b27f32dabc851645578c7edd
James Qian <sotux82@gmail.com>
2018-11-20 18:55:40 +0800

    kodexplorer: add Kod Explorer installation script
    After install this package, run kod_install.sh to install Kod Explorer
    on OpenWrt. It needs an external USB disk to install.
    
    Signed-off-by: James Qian <sotux82@gmail.com>

---
+++ packages/kodexplorer/kod_install.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+
+NAME=KodExplorer
+VER=4.36
+URL=https://github.com/kalcaddle/KodExplorer/archive
+MNT_DIR=`uci get fstab.@mount[0].target`
+LAN_IP=`uci get network.lan.ipaddr`
+KOD_INSTALL_DIR=$MNT_DIR/$NAME
+DATA_DIR=$MNT_DIR/data
+NGINX_CONF_NAME=$NAME.conf
+PHP7_FPM_CONF_NAME=$NAME.conf
+
+func_generate_nginx_conf()
+{
+	[ ! -d /etc/nginx/conf.d ] && mkdir -p /etc/nginx/conf.d
+	cat > "/etc/nginx/conf.d/$NGINX_CONF_NAME" <<EOF
+server {
+    listen 8080;
+    server_name localhost;
+    access_log /var/log/nginx/KodExplorer_access.log;
+    error_log /var/log/nginx/KodExplorer_error.log;
+
+    location / {
+        root $KOD_INSTALL_DIR;
+        index index.php;
+    }
+    
+    location ~ \.php\$ {
+        root $KOD_INSTALL_DIR;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_pass unix:/var/run/$NAME-php7-fpm.sock;
+        include fastcgi_params;
+    }
+}
+EOF
+}
+
+func_generate_php7_fpm_conf()
+{
+	cat > "/etc/php7-fpm.d/$PHP7_FPM_CONF_NAME" <<EOF
+[$NAME]
+user = nobody
+group = nogroup
+listen = /var/run/$NAME-php7-fpm.sock
+listen.mode = 0666
+listen.allowed_clients = 127.0.0.1
+pm = dynamic
+pm.max_children = 5
+pm.start_servers = 2
+pm.min_spare_servers = 1
+pm.max_spare_servers = 3
+pm.max_requests = 500
+chdir = /
+php_admin_value[upload_max_filesize] = 16M
+php_admin_value[memory_limit] = 24M
+php_admin_value[doc_root] = $KOD_INSTALL_DIR
+php_admin_value[open_basedir] = $KOD_INSTALL_DIR/:$DATA_DIR/:/tmp/:/proc/
+EOF
+}
+
+func_install_kodexplorer()
+{
+	echo "Installing, please wait..."
+
+	tar xzf $MNT_DIR/$NAME-$VER.tar.gz -C $MNT_DIR
+	rm $MNT_DIR/$NAME-$VER.tar.gz
+
+	mv $MNT_DIR/$NAME-$VER $KOD_INSTALL_DIR
+	mv $KOD_INSTALL_DIR/data $DATA_DIR
+	echo -n "<?php define('DATA_PATH','$DATA_DIR/');" > $KOD_INSTALL_DIR/config/define.php
+
+	chown nobody:nogroup -R $KOD_INSTALL_DIR
+	chown nobody:nogroup -R $DATA_DIR
+	chmod 775 -R $KOD_INSTALL_DIR
+	chmod 775 -R $DATA_DIR
+
+	func_generate_nginx_conf
+	func_generate_php7_fpm_conf
+
+	/etc/init.d/php7-fpm restart
+	/etc/init.d/nginx restart
+
+	echo "Kod Explorer install finished. Please visit http://$LAN_IP:8080"
+}
+
+if [ ! -d $MNT_DIR ]; then
+	echo "$MNT_DIR does not exit."
+	exit 0
+fi
+
+if [ -d $KOD_INSTALL_DIR ]; then
+	echo "Kod Explorer has been installed in $KOD_INSTALL_DIR, exit installation."
+	exit 0
+fi
+
+# now download kod explorer
+wget --no-check-certificate $URL/$VER.tar.gz -O $MNT_DIR/$NAME-$VER.tar.gz
+
+if [ $? != 0 ]; then
+	echo "Download Kod Explorer failed."
+	exit 0
+fi
+
+if [ ! -e $MNT_DIR/$NAME-$VER.tar.gz ]; then
+	echo "Download Kod Explorer failed." 
+else
+	func_install_kodexplorer
+fi
+
+exit 0
