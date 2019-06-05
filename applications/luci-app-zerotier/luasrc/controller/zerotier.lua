43facf5ca7789d97d3530404f5dac13a8aec0659
James Qian <sotux82@gmail.com>
2019-06-05 17:17:54 +0800

    luci-app-zerotier: move the entry from vpn to services
    Signed-off-by: Zheng Qian <sotux82@gmail.com>

--- applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+++ applications/luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -2,21 +2,18 @@ module("luci.controller.zerotier",package.seeall)
 
 function index()
   if not nixio.fs.access("/etc/config/zerotier")then
-return
-end
+    return
+  end
 
-entry({"admin","vpn"}, firstchild(), "VPN", 45).dependent = false
-
-entry({"admin", "vpn", "zerotier"},firstchild(), _("ZeroTier")).dependent = false
-
-entry({"admin", "vpn", "zerotier", "general"},cbi("zerotier/settings"), _("Base Setting"), 1)
-entry({"admin", "vpn", "zerotier", "log"},form("zerotier/info"), _("Interface Info"), 2)
-
-entry({"admin","vpn","zerotier","status"},call("act_status"))
+  entry({"admin", "services"}, firstchild(), "ZeroTier", 45).dependent = false
+  entry({"admin", "services", "zerotier"}, firstchild(), _("ZeroTier")).dependent = false
+  entry({"admin", "services", "zerotier", "general"}, cbi("zerotier/settings"), _("Base Setting"),   1)
+  entry({"admin", "services", "zerotier", "log"},     form("zerotier/info"),    _("Interface Info"), 2)
+  entry({"admin", "services", "zerotier", "status"},  call("act_status"))
 end
 
 function act_status()
-local e={}
+  local e={}
   e.running=luci.sys.call("pgrep /usr/bin/zerotier-one >/dev/null")==0
   luci.http.prepare_content("application/json")
   luci.http.write_json(e)
