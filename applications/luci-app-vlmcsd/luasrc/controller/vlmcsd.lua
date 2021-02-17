89b773cfc1a88f1cd035f4c38d8628e371561f3f
Zheng Qian <sotux82@gmail.com>
2021-02-17 22:43:46 +0800

    luci-app-vlmcsd: fix menu
    Signed-off-by: Zheng Qian <sotux82@gmail.com>

--- applications/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
+++ applications/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
@@ -4,14 +4,16 @@ function index()
 	if not nixio.fs.access("/etc/config/vlmcsd") then
 		return
 	end
-	entry({"admin", "services", "vlmcsd"},
-		alias("admin", "services", "vlmcsd", "general"), 
-		_("vlmcsd"), 10).dependent = true
-	
+	local e = entry({"admin", "services", "vlmcsd"},
+		alias("admin", "services", "vlmcsd", "general"),
+		_("vlmcsd"), 10)
+	e.dependent = true
+	e.acl_depends = { "luci-app-vlmcsd" }
+
 	entry({"admin", "services", "vlmcsd", "general"},
 		cbi("vlmcsd/general"),
 		_("General Settings"), 10).leaf = true
-		
+
 	entry({"admin", "services", "vlmcsd", "config"},
 		form("vlmcsd/configfile"),
 		_("Config File"), 20).leaf = true
